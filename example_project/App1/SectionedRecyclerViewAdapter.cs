c03be7a38e0576629b247083c81610ed936d4795
Randall Schmidt <randallschmidt1@gmail.com>
2019-02-10 09:12:51 +0700

    merge

--- example_project/App1/SectionedRecyclerViewAdapter.cs
+++ example_project/App1/SectionedRecyclerViewAdapter.cs
@@ -1,4 +1,27 @@
-﻿using Android.Support.V7.Widget;
+﻿/*
+ * MIT License
+
+* Copyright (c) 2017 mistval
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE. */
+
+using Android.Support.V7.Widget;
 using Android.Views;
 
 namespace Controls
@@ -28,6 +51,12 @@ namespace Controls
                 return itemCount + sectionCount;
             }
         }
+
+        #endregion
+
+        #region Properties
+
+        protected abstract int SectionCount { get; }
 
         #endregion
 
@@ -96,8 +125,6 @@ namespace Controls
             position += item;
             return position;
         }
-
-        protected abstract int SectionCount { get; }
 
         protected abstract int GetItemCountForSection(int section);
 
